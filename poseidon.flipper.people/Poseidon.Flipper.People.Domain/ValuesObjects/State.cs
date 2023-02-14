24f276dbf72ca5a24be53fc96bb58a3a2cd96e1e
Ninshows <124107953+Ninshows@users.noreply.github.com>
2023-02-13 22:32:53 -0300

    criação da classe State

---
+++ poseidon.flipper.people/Poseidon.Flipper.People.Domain/ValuesObjects/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poseidon.Flipper.People.Domain.ValuesObjects
+{
+    public class State
+    {
+
+        public string Description { get; private set; }
+
+        public State(string description)
+        {
+            Description = description;
+        }
+        
+    }
+}
